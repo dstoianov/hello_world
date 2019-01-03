b204005c652fec6be80aca0a585dcfa7f74bf5fa
Jeff Bradberry <jeff.bradberry@gmail.com>
2019-01-03 11:45:54 -0500

    Changes to unbreak initial app

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -17,9 +17,17 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='base.html')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
